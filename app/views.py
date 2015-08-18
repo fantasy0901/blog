e134da25e9ba3cd65e783a790734c571713ec4a1
root <root@bolometest-VirtualBox.(none)>
2015-08-18 17:26:45 +0800

    app.db

--- app/views.py
+++ app/views.py
@@ -6,7 +6,9 @@ from .forms import LoginForm
 #@app.route('/index')
 def login():
 	form = LoginForm()
-	return render_template('login.html',title='Sign In',form=form)
+	if form.validate_on_submit():
+		flash('login requested for OpenID="' + form.openid.date + '", remember_me=' + str(form.remember_me.data))
+	return render_template('login.html',title='Sign In',form=form,providers = app.config['OPENID_PROVIDERS'])
 	#user = { 'nickname': 'Fantasy'}
 	#posts = [ 
 	#     {
