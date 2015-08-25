9461c114a0c45700239aa1f475bf0c523f16b3fa
root <root@bolometest-VirtualBox.(none)>
2015-08-25 19:29:22 +0800

    views.py models.py base.html login.html

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
+#!flask/bin/python
 from app import db
-
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
-import app import app, db, lm, oid
+#!flask/bin/python
 from flask import render_template,flash,redirect,session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
-from flask.ext.login import login_user, logout_user, current_user, login_required
-
 @lm.user_loader
 def load_user(id):
 	return User.query.get(int(id))
@@ -12,28 +12,11 @@ def load_user(id):
 def before_request():
 	g.user = current_user
 
-@app.route('/login',methods = ['GET','POST'])
-#oid.loginhandler
-def login():
-	if g.user is not None and g.user.is_authenticated():
-		return redirect(url_for('index'))
-	form = LoginForm()
-	if form.validate_on_submit():
-		session['remember_me'] = form.remember_me.data
-		return oid.try_login(form.openid.data, ask_for=['nickname','email'])
-	return render_template('login.html',title='Sign In',form=form,providers = app.config['OPENID_PROVIDERS'])
-
-@app.route('/logout')
-def logout():
-	logout_user()
-	return redirect(url_for('index'))
-	
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
 	user = g.user
-	#user = { 'nickname': 'Fantasy'}
 	posts = [ 
 	     {
 		'author': {'nickname':'John'},
@@ -46,7 +29,18 @@ def index():
 	]
 	return render_template("index.html",title='Home',user = user,posts = posts)
 
-@login.after_login
+@app.route('/login',methods = ['GET','POST'])
+@oid.loginhandler
+def login():
+	if g.user is not None and g.user.is_authenticated():
+		return redirect(url_for('index'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		session['remember_me'] = form.remember_me.data
+		return oid.try_login(form.openid.data, ask_for=['nickname','email'])
+	return render_template('login.html',title='Sign In',form=form,providers = app.config['OPENID_PROVIDERS'])
+	
+@oid.after_login
 def after_login(resp):
 	if resp.email is None or resp.email=="":
 		flash('Invalid login. Please try again.')
@@ -65,3 +59,8 @@ def after_login(resp):
 		session.pop('remember_me', None)
 	login_user(user, remember=remember_me)
 	return redirect(request.args.get('next') or url_for('index'))
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
